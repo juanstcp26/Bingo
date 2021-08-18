51b957c007bebb4a4771272164836f6c3a153591
juanstcp26 <juanstcp26@gmail.com>
2021-08-18 17:13:39 -0500

    Generación de numero con letra

--- bingo.js
+++ bingo.js
@@ -4,7 +4,12 @@ var sigue,letra;
 
 do{
     numero = Math.round(Math.random() * (numInicial - numFinal) + numFinal); // Número aleatorio
-    
-    console.log(numero);
+    // Letras
+    if (numero >= 1 && numero <= 15) letra = "B";
+    if (numero >= 16 && numero <= 30) letra = "I";
+    if (numero >= 31 && numero <= 45) letra = "N";
+    if (numero >= 46 && numero <= 60) letra = "G";
+    if (numero >= 61 && numero <= 75) letra = "O";
+    console.log(letra+numero);
     sigue = prompt("Desea sacar otra bola? S/N");
 } while (sigue == "S" || sigue == "s")
